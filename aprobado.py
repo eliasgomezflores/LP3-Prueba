0cccb16fd18a93b9350b7cc8fea44431e8c24148
Orlando <orlando97rm@gmail.com>
2020-07-09 14:59:16 -0500

    Método para determinar aprobado.

---
+++ aprobado.py
@@ -0,0 +1,12 @@
+
+
+def determinarAprobado(promedio):
+    
+    if 0<=promedio<=10:
+        print("El alumno está desaprobado.")
+        
+    elif 20>=promedio>10:
+        print("El alumno está aprobado.")
+    
+    else:
+        print("La entrada es incorrecta.")
